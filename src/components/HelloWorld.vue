6af79fb18fa037c7280f35726ac0b13bad57470d
Naeem Mujeeb <leguy@gmail.com>
2020-06-07 23:37:27 +0100

    upload

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,66 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div id="svgMap"><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/svgmap/dist/svgMap.min.css"></div>
 </template>
 
+
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/svgmap/dist/svgMap.min.js"></script>
+
 <script>
+import svgMap from "svgmap";
+// import svgPanZoom from "svgpanzoom";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  mounted() {
+    new svgMap({
+      targetElementID: "svgMap",
+      data: {
+        data: {
+          news: {
+            name: "Latest Update",
+            format : "{0}",
+
+            
+          },
+          gdp: {
+            name: "GDP per capita",
+            format: "{0}%",
+            thousandSeparator: ",",
+            thresholdMax: 50000,
+            thresholdMin: 1000
+          },
+          change: {
+            name: "Change to year before",
+            format: "{0} %"
+          }
+        },
+        applyData: "gdp",
+        values: {
+          AF: { gdp: 587, change: 4.73 },
+          AL: { gdp: 4583, change: 11.09 },
+          DZ: { gdp: 4293, change: 10.01 },
+          GB: { gdp: 3000, change: 10.01, news : ["", "asdads"]}
+          // ...
+        }
+      }
+    });
   }
-}
+};
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+#svgMap {
+  width: 800px;
+  height: 600px;
+}
+
 h3 {
   margin: 40px 0 0;
 }
